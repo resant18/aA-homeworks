97df399c1b2ee76e5c8f67102adf60c13c4cc204
resant18 <santoso.renata@gmail.com>
2019-09-29 01:09:51 -0700

    Add W7D1 RailsAuth

---
+++ W7D1/RailsAuth/app/models/user.rb
@@ -0,0 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+    validates :username, presence:true, uniqueness: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    before_validation :ensure_session_token
+
+    attr_reader :password 
+
+    def self.find_by_credentials(username, password)
+        user = User.findby(username: username)
+        return user if user && valid_password?(password)
+        nil
+    end
+
+    def valid_password?(password_digest)
+        BCrypt::Password.new(self.password_digest).is_password?(password_digest)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)        
+    end
+
+    def reset_session_token!
+        self.session_token = self.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def password=(pw)
+        @password = pw
+        self.password_digest = BCrypt::Password.create(pw)        
+    end
+
+    private
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+end
